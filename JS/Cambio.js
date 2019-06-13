41231ab3562b0f624726955cec90dc8a80c2b0cb
UO257192 <UO257192@uniovi.es>
2019-06-13 09:28:28 +0200

    FINAL

--- JS/Cambio.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-class Cambio{
-    cambiar(){
-        $("#navbar").hide();
-        // document.getElementById("aux").innerHTML = "<div class='welcome'><h2>¡BIENVENIDO A LA AMBORGESERIA!</h2><button  onclick='nav.login()'>Inicia sesion >></button></div>";
-    }
-}
-var cambiador = new Cambio();
